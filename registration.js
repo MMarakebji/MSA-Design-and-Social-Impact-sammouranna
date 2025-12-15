667b335da60f92d0831613fff982dc754b7ce751
elhajahmad75-alt <elhajahmad75@gmail.com>
2025-12-15 12:05:16 +0200

    m

--- registration.js
+++ registration.js
@@ -10,3 +10,24 @@ cards.forEach(card => {
         card.classList.toggle("active");
     });
 });
+const vid = document.querySelector(".hero-video");
+const btn = document.getElementById("soundBtn");
+
+btn.addEventListener("click", async () => {
+  vid.muted = false;
+  vid.volume = 1;           // 0 to 1
+  await vid.play();         // ensures it plays after user gesture
+  btn.style.display = "none";
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("heroRegisterBtn");
+  const target = document.getElementById("register");
+
+  if (!btn) return console.log("❌ hero button not found");
+  if (!target) return console.log("❌ #register section not found");
+
+  btn.addEventListener("click", () => {
+    target.scrollIntoView({ behavior: "smooth", block: "start" });
+  });
+});
